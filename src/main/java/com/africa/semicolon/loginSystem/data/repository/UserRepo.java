356bd7ddf89660525efc883aa7f492a85c22842f
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-04-02 19:27:22 +0530

    my login system

---
+++ src/main/java/com/africa/semicolon/loginSystem/data/repository/UserRepo.java
@@ -0,0 +1,12 @@
+package com.africa.semicolon.loginSystem.data.repository;
+
+
+import com.africa.semicolon.loginSystem.data.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Component
+@Repository
+public interface UserRepo extends MongoRepository<User,String> {
+}
