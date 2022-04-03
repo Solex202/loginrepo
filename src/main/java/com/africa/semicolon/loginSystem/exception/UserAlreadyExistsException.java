597819c190e77c004d22fa5b0f2b9f044e995b25
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-04-03 18:59:46 +0100

    my login system

---
+++ src/main/java/com/africa/semicolon/loginSystem/exception/UserAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.africa.semicolon.loginSystem.exception;
+
+public class UserAlreadyExistsException extends RuntimeException{
+
+
+    public UserAlreadyExistsException(String msg) {
+
+    }
+}
