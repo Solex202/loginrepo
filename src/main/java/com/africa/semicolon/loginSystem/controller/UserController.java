e69e2fc871d304be3bcd8fb99174f90c62f961cb
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-04-10 08:58:44 +0100

    my login system

--- src/main/java/com/africa/semicolon/loginSystem/controller/UserController.java
+++ src/main/java/com/africa/semicolon/loginSystem/controller/UserController.java
@@ -2,12 +2,20 @@ package com.africa.semicolon.loginSystem.controller;
 
 
 import com.africa.semicolon.loginSystem.data.model.User;
+import com.africa.semicolon.loginSystem.dtos.ApiResponse;
 import com.africa.semicolon.loginSystem.dtos.request.CreateUserRequest;
 import com.africa.semicolon.loginSystem.dtos.request.LoginRequest;
 import com.africa.semicolon.loginSystem.dtos.response.CreateUserResponse;
 import com.africa.semicolon.loginSystem.dtos.response.LoginResponse;
+import com.africa.semicolon.loginSystem.exception.IncorrectPasswordException;
+import com.africa.semicolon.loginSystem.exception.InvalidPasswordException;
+import com.africa.semicolon.loginSystem.exception.UserAlreadyExistsException;
+import com.africa.semicolon.loginSystem.exception.UserNotFoundException;
+
 import com.africa.semicolon.loginSystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,13 +28,29 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/createUser")
-    public CreateUserResponse response(@RequestBody CreateUserRequest request){
-     return userService.createUser(request) ;
+    public ResponseEntity<?> response(@RequestBody CreateUserRequest request){
+        try{
+            return new ResponseEntity<>(userService.createUser(request), HttpStatus.OK);
+        }catch(UserAlreadyExistsException ex){
+            return new ResponseEntity<>(new ApiResponse(false, ex.getMessage()), HttpStatus.ALREADY_REPORTED);
+        }
+        catch(InvalidPasswordException ex){
+            return new ResponseEntity<>( new ApiResponse(false, ex.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+
     }
 
     @PostMapping("/login")
-    public LoginResponse response(@RequestBody LoginRequest request){
-        return userService.login(request) ;
+    public ResponseEntity<?> response(@RequestBody LoginRequest request){
+        try{
+            return new ResponseEntity<>(userService.login(request), HttpStatus.OK);
+        }catch(IncorrectPasswordException ex){
+            return new ResponseEntity<>(new ApiResponse(false, ex.getMessage()), HttpStatus.UNAUTHORIZED);
+        }
+        catch(UserNotFoundException ex){
+            return new ResponseEntity<>(new ApiResponse(false, ex.getMessage()), HttpStatus.NOT_FOUND);
+        }
+
     }
 
     @GetMapping("/getUsers")
