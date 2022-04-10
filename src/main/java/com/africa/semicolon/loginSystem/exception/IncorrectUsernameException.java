3d926370767ef91707a6e153f8b3c015076bfdfa
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-04-10 21:24:33 +0100

    my login system

---
+++ src/main/java/com/africa/semicolon/loginSystem/exception/IncorrectUsernameException.java
@@ -0,0 +1,8 @@
+package com.africa.semicolon.loginSystem.exception;
+
+public class IncorrectUsernameException extends RuntimeException{
+
+    public IncorrectUsernameException(String msg){
+        super(msg);
+    }
+}
