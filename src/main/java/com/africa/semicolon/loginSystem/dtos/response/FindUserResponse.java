7c85b3d6be467065e0d704c05f1e2d8d8e6f8a20
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-04-12 11:26:58 +0100

    my login page

---
+++ src/main/java/com/africa/semicolon/loginSystem/dtos/response/FindUserResponse.java
@@ -0,0 +1,12 @@
+package com.africa.semicolon.loginSystem.dtos.response;
+
+
+import lombok.Data;
+
+@Data
+public class FindUserResponse {
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+}
