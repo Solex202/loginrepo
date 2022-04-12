c7f36f35407022c7f283fdbb45e50e7167a109c5
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-04-12 19:03:44 +0100

    my login app

---
+++ src/main/java/com/africa/semicolon/loginSystem/dtos/response/DeleteResponse.java
@@ -0,0 +1,13 @@
+package com.africa.semicolon.loginSystem.dtos.response;
+
+
+import lombok.Data;
+
+@Data
+public class DeleteResponse {
+
+    private String message;
+//    private String firstName;
+//    private String lastName;
+//    private String userName;
+}
