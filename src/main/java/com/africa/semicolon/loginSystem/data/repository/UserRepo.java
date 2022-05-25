62f5452334dbc81d952136cd21becdbed01f0928
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-25 22:08:40 +0100

    new commit

--- src/main/java/com/africa/semicolon/loginSystem/data/repository/UserRepo.java
+++ src/main/java/com/africa/semicolon/loginSystem/data/repository/UserRepo.java
@@ -6,6 +6,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Component
 @Repository
 public interface UserRepo extends MongoRepository<User,String> {
